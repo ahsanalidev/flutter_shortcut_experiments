159ad9e384b7e72466564a63f1eb3866fa7eac36
Ahsan Ali <mrahsanali5@gmail.com>
2021-04-25 09:24:20 +0500

    changes

---
+++ lib/global_shortcut.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class TestIntent extends Intent {
+  TestIntent();
+}
+
+class GlobalShortcut extends StatelessWidget {
+  GlobalShortcut({
+    Key key,
+    this.child,
+    this.focusNode,
+  }) : super(key: key);
+  final Widget child;
+  final _shortcuts = {
+    LogicalKeySet(LogicalKeyboardKey.arrowDown): TestIntent(),
+  };
+
+  final FocusNode focusNode;
+  final _actions = {
+    TestIntent: CallbackAction(onInvoke: (_) => print('f1 pressed'))
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return FocusableActionDetector(
+      shortcuts: _shortcuts,
+      actions: _actions,
+      child: child,
+    );
+  }
+}
